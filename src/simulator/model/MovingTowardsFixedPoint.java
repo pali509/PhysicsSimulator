669a9397baf5b5301e87ff16d1050460ce39b629
pablo <pablo@LAPTOP-LLS0T211>
2021-03-24 19:55:57 +0100

    Added the classes missing

--- src/simulator/model/MovingTowardsFixedPoint.java
+++ src/simulator/model/MovingTowardsFixedPoint.java
@@ -8,18 +8,30 @@ public class MovingTowardsFixedPoint implements ForceLaws {
 	
 	Vector2D c;
 	double g;
-
-	@Override
-	public void apply(List<Body> bs) {
-		
-
-	}
 	
 	public MovingTowardsFixedPoint(Vector2D c, double g) {
 		this.c = c;
 		this.g = g;
+	}
+	
+	@Override
+	public void apply(List<Body> bs) {
+		for(int i = 0; i < bs.size(); i++) {
+			bs.get(i).addForce(getForce(bs.get(i)));
+		}
+
+	}
+	
+	private Vector2D getForce(Body i) {
+		Vector2D force;
+		Vector2D d;
 		
+		d = new Vector2D();
+		force = new Vector2D();
 		
+		d = c.minus(i.getPosition());
+		force = d.scale(i.getMass()/g);
+		
+		return force;
 	}
 
-}
--- src/simulator/model/NewtonUniversalGravitation.java
+++ src/simulator/model/NewtonUniversalGravitation.java
@@ -7,34 +7,44 @@ import simulator.misc.Vector2D;
 public class NewtonUniversalGravitation implements ForceLaws {
 	
 	double g;
-
-	@Override
-	public void apply(List<Body> bs) {
-		for(int i = 0; i < bs.size(); i++) {
-			for(int j = 0; j < bs.size(); j++) {
-				if(i != j) {
-					
-					bs.get(i).addForce();
-					
-				}
-				
-			}
-		}
-		
-	}
 	
 	public NewtonUniversalGravitation(double g) {
 		this.g = g;
 	}
 	
-	//TODO
+	@Override
+	public void apply(List<Body> bs) {
+		g = 1;
+		for(int i = 0; i < bs.size(); i++) {
+			for(int j = 0; j < bs.size(); j++) {
+				if(i != j) 					
+					bs.get(i).addForce(getForce(bs.get(i), bs.get(j)));
+			}
+		}
+		
+	}
 	
 	public Vector2D getForce(Body i, Body j) {
+		Vector2D d;
+		Vector2D objForce;
+		double f;
+		double m;
 		
+		d = new Vector2D();
+		objForce = new Vector2D();
 		
+		d = j.getPosition().minus(i.getPosition()); //Calculate the distance between the two bodies
+		m = d.magnitude() * d.magnitude(); //calculate the magnitude between the two bodies ^ 2
 		
+		if(m > 0)
+			f = g * ((i.getMass() * j.getMass()/m)); //formula given to us
+		else {
+			f = 0.0;
+		}
 		
-		return null;
+		objForce = d.scale(f); //formula given to us, (dx * f, dy * f)
+		
+		return objForce;
 	}
 
 }
